a5d41015882bef644ff30732bb21bde502a38211
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2022-09-18 22:37:56 +0430

    [INIT]

---
+++ src/Providers/LaralyticsProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Arashrasoulzadeh\Laralytics\Providers;
+use Illuminate\Support\ServiceProvider;
+
+class LaralyticsProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
