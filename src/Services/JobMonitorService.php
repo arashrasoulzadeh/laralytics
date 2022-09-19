5879c885fbdfcb48f1990bcd5a419f60f5cdf0ee
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2022-09-19 13:28:05 +0430

    [ADD] controllers

--- src/Services/JobMonitorService.php
+++ src/Services/JobMonitorService.php
@@ -4,5 +4,41 @@ namespace ArashRasoulzadeh\Laralytics\Services;
 
 class JobMonitorService
 {
+    private var $presenters = [];
+
+    /**
+     * @return array
+     */
+    public function getPresenters()
+    {
+        return $this->presenters;
+    }
+
+    /**
+     * @param array $presenters
+     */
+    public function setPresenters( $presenters )
+    {
+        $this->presenters = $presenters;
+    }
+
+    /**
+     * @param array $presenter
+     */
+    public function addPresenter( $presenter )
+    {
+        $this->presenters[] = $presenter;
+    }
+
+    public  function present()
+    {
+        $response = '';
+        foreach ( $this->getPresenters() as $presenter )
+        {
+            $response.=$presenter.'\n';
+        }
+        return $response;
+    }
+
 
 }
